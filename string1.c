6337d08bc322647e29492d6d71f0e8bc23ca72de
Fatemah Adnan <141007927+FatmaBindarey@users.noreply.github.com>
2023-09-06 19:46:38 +0300

    Add files via upload

---
+++ string1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    char str[100];
+    printf(" enter sentence :");
+    scanf(" %s ",&str);
+    for ( i=0;str[i]!='\0';++i)
+        printf(" enter string : %d",i);
+
+}
